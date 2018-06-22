cfd559ffd3381d3b7cb78af90174001ad0d23aee
Austin Gorman <austin.gorman@gmail.com>
2018-06-21 21:51:46 -0500

    finished the project

--- powers.js
+++ powers.js
@@ -1,12 +1,24 @@
 /*
-    In the following code, replace `event` with the correct string to handle
-    a click event on the button. Then write the function that has instructions
-    for activating the corresponding power. The function should...
-        1. Remove the `disabled` class
-        2. Add the `enabled` class
-
+In the following code, replace `event` with the correct string to handle
+a click event on the button. Then write the function that has instructions
+for activating the corresponding power. The function should...
+1. Remove the `disabled` class
+2. Add the `enabled` class
 */
-document.querySelector("#activate-flight").addEventListener(event, handlerFunction)
+const flight = document.querySelector("#flight")
+const mindreading = document.querySelector("#mindreading")
+const xray = document.querySelector("#xray")
+const section = document.querySelectorAll("section")
+
+
+function changeClass(buttonName, newClass, anotherClass) {
+  buttonName.classList.toggle(newClass)
+  buttonName.classList.toggle(anotherClass)
+}
+
+document.querySelector("#activate-flight").addEventListener("click", function () {
+  changeClass(flight, "power-disabled", "power-enabled")
+})
 
 /*
     Now write two more event handlers that activate the other two powers
@@ -14,8 +26,31 @@ document.querySelector("#activate-flight").addEventListener(event, handlerFuncti
 */
 
 
+document.querySelector("#activate-mindreading").addEventListener("click", function () {
+    changeClass(mindreading, "power-disabled", "power-enabled")
+})
+
+document.querySelector("#activate-xray").addEventListener("click", function () {
+    changeClass(xray, "power-disabled", "power-enabled")
+})
+
+
 /*
     Write two more event handlers for activating and deactivating all powers
     when the corresponding buttons are clicked. You will need to use the
     `document.querySelectorAll()` method for these.
 */
+
+document.querySelector("#activate-all").addEventListener("click", function() {
+    section.forEach(function(power){
+        power.className = ""
+        power.classList.toggle("power-enabled")
+    })
+})
+
+document.querySelector("#deactivate-all").addEventListener("click", function () {
+    section.forEach(function (power) {
+        power.className = ""
+        power.classList.toggle("power-disabled")
+    })
+})
